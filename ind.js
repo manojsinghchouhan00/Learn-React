52abe4d6e816bfc762e7e751a50e60743ba9713a
manojsinghchouhan00 <manojsinghchouhan00@gmail.com>
2024-02-10 18:39:11 +0530

    initial commit

---
+++ ind.js
@@ -0,0 +1,23 @@
+class Shap {
+    constructor(name) {
+       this.shapName = name;
+    }
+   
+    area() {
+       console.log('I have a ' + this.shapName);
+    }
+ }
+ 
+ class Circle extends Shap {
+      constructor(name, r) {
+           super(name);
+           this.radius = r;
+      }
+      show() {
+        super.area()
+           console.log("Area of "+ this.shapName+', is a '+ (this.radius*this.radius*3.14))
+      }
+ }
+ 
+ const myShap = new Circle("Circle-1", 10);
+ myShap.show();
